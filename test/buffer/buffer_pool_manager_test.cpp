c4e1406be3bafd7f353722e05bd2cb422e870194
ysj1173886760 <1173886760@qq.com>
2022-05-01 11:09:27 +0800

    fix unit test on bpm

--- test/buffer/buffer_pool_manager_test.cpp
+++ test/buffer/buffer_pool_manager_test.cpp
@@ -20,13 +20,12 @@ TEST(BufferPoolManagerTest, BinaryDataTest) {
     auto disk_manager = new DiskManager(filename);
     auto bpm = new BufferPoolManager(buffer_pool_size, disk_manager);
 
-    page_id_t page_id_tmp;
-    auto page0 = bpm->NewPage(&page_id_tmp);
+    int page_id[buffer_pool_size * 2];
+    auto page0 = bpm->NewPage(&page_id[0]);
 
+    // we shouldn't have any assumption on the page number, since it's decided by disk manager
     // buffer pool is empty, we should be able to create a new page
-    // and page id is 0
     EXPECT_NE(nullptr, page0);
-    EXPECT_EQ(0, page_id_tmp);
 
     char random_binary_data[PAGE_SIZE];
     // Generate random binary data
@@ -43,28 +42,30 @@ TEST(BufferPoolManagerTest, BinaryDataTest) {
 
     // we should be able to allocate there pages
     for (size_t i = 1; i < buffer_pool_size; i++) {
-        EXPECT_NE(nullptr, bpm->NewPage(&page_id_tmp));
+        EXPECT_NE(nullptr, bpm->NewPage(&page_id[i]));
     }
 
     // we should not be able to allocate any new pages
-    for (size_t i = 0; i < buffer_pool_size; i++) {
-        EXPECT_EQ(nullptr, bpm->NewPage(&page_id_tmp));
+    for (size_t i = buffer_pool_size; i < buffer_pool_size * 2; i++) {
+        EXPECT_EQ(nullptr, bpm->NewPage(&page_id[i]));
     }
 
     // unpin 5 pages and allocate 5 new pages
     // which will lead the previous pages swapped out to disk
     for (int i = 0; i < 5; i++) {
-        EXPECT_EQ(bpm->UnpinPage(i, true), true);
-    }
-    for (int i = 0; i < 5; i++) {
-        EXPECT_NE(bpm->NewPage(&page_id_tmp), nullptr);
-        bpm->UnpinPage(page_id_tmp, false);
+        EXPECT_EQ(bpm->UnpinPage(page_id[i], true), true);
     }
 
-    // fetch page 0 from disk again
-    page0 = bpm->FetchPage(0);
+    int more_pages[5];
+    for (int i = 0; i < 5; i++) {
+        EXPECT_NE(bpm->NewPage(&more_pages[i]), nullptr);
+        bpm->UnpinPage(more_pages[i], false);
+    }
+
+    // fetch first page from disk again
+    page0 = bpm->FetchPage(page_id[0]);
     EXPECT_EQ(0, std::memcmp(page0->GetData(), random_binary_data, PAGE_SIZE));
-    EXPECT_EQ(true, bpm->UnpinPage(0, true));
+    EXPECT_EQ(true, bpm->UnpinPage(page_id[0], true));
 
     delete bpm;
     delete disk_manager;
