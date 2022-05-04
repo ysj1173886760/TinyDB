faaa67119d81f7fe6d7bdc6376390680ace3ae5b
ysj1173886760 <1173886760@qq.com>
2022-05-04 20:55:33 +0800

    add test for tinyint

--- test/type/tinyint_type_test.cpp
+++ test/type/tinyint_type_test.cpp
@@ -28,6 +28,8 @@ TEST(TinyintTest, BasicTest) {
     auto product = x.Multiply(y);
     auto quot = x.Divide(y);
     auto rem = x.Modulo(y);
+    auto maxx = x.Max(y);
+    auto minn = x.Min(y);
 
     // internal value test
     EXPECT_EQ(sum.GetAs<int8_t>(), static_cast<int8_t>(20));
@@ -35,6 +37,56 @@ TEST(TinyintTest, BasicTest) {
     EXPECT_EQ(product.GetAs<int8_t>(), static_cast<int8_t>(91));
     EXPECT_EQ(quot.GetAs<int8_t>(), static_cast<int8_t>(1));
     EXPECT_EQ(rem.GetAs<int8_t>(), static_cast<int8_t>(6));
+    EXPECT_EQ(maxx.GetAs<int8_t>(), static_cast<int8_t>(13));
+    EXPECT_EQ(minn.GetAs<int8_t>(), static_cast<int8_t>(7));
+    
+    // compare test
+    EXPECT_EQ(x.CompareEquals(y), CmpBool::CmpFalse);
+    EXPECT_EQ(x.CompareNotEquals(y), CmpBool::CmpTrue);
+    EXPECT_EQ(x.CompareGreaterThan(y), CmpBool::CmpTrue);
+    EXPECT_EQ(x.CompareGreaterThanEquals(y), CmpBool::CmpTrue);
+    EXPECT_EQ(x.CompareLessThan(y), CmpBool::CmpFalse);
+    EXPECT_EQ(x.CompareLessThanEquals(y), CmpBool::CmpFalse);
+
+    // value test
+    EXPECT_EQ(sum.CompareEquals(Value(TypeId::TINYINT, static_cast<int8_t>(20))), CmpBool::CmpTrue);
+    EXPECT_EQ(diff.CompareEquals(Value(TypeId::TINYINT, static_cast<int8_t>(6))), CmpBool::CmpTrue);
+    EXPECT_EQ(product.CompareEquals(Value(TypeId::TINYINT, static_cast<int8_t>(91))), CmpBool::CmpTrue);
+    EXPECT_EQ(quot.CompareEquals(Value(TypeId::TINYINT, static_cast<int8_t>(1))), CmpBool::CmpTrue);
+    EXPECT_EQ(rem.CompareEquals(Value(TypeId::TINYINT, static_cast<int8_t>(6))), CmpBool::CmpTrue);
+    EXPECT_EQ(maxx.CompareEquals(Value(TypeId::TINYINT, static_cast<int8_t>(13))), CmpBool::CmpTrue);
+    EXPECT_EQ(minn.CompareEquals(Value(TypeId::TINYINT, static_cast<int8_t>(7))), CmpBool::CmpTrue);
+}
+
+TEST(TinyintTest, NullTest) {
+    auto x = Value(TypeId::TINYINT, static_cast<int8_t>(0));
+    auto y = Type::Null(TypeId::TINYINT);
+
+    auto sum = x.Add(y);
+    auto diff = x.Subtract(y);
+    auto product = x.Multiply(y);
+    auto quot = x.Divide(y);
+    auto rem = x.Modulo(y);
+    auto maxx = x.Max(y);
+    auto minn = x.Min(y);
+
+    EXPECT_EQ(x.IsZero(), true);
+    EXPECT_EQ(y.IsNull(), true);
+    EXPECT_EQ(sum.IsNull(), true);
+    EXPECT_EQ(diff.IsNull(), true);
+    EXPECT_EQ(product.IsNull(), true);
+    EXPECT_EQ(quot.IsNull(), true);
+    EXPECT_EQ(rem.IsNull(), true);
+    EXPECT_EQ(maxx.IsNull(), true);
+    EXPECT_EQ(minn.IsNull(), true);
+    
+    // compare test
+    EXPECT_EQ(x.CompareEquals(y), CmpBool::CmpNull);
+    EXPECT_EQ(x.CompareNotEquals(y), CmpBool::CmpNull);
+    EXPECT_EQ(x.CompareGreaterThan(y), CmpBool::CmpNull);
+    EXPECT_EQ(x.CompareGreaterThanEquals(y), CmpBool::CmpNull);
+    EXPECT_EQ(x.CompareLessThan(y), CmpBool::CmpNull);
+    EXPECT_EQ(x.CompareLessThanEquals(y), CmpBool::CmpNull);
 }
 
 }
