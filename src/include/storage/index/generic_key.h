ec3a2678aa77fba1b9db1927d4e7a22c461105e3
ysj1173886760 <1173886760@qq.com>
2022-05-12 21:53:07 +0800

    implement generic key and comparator

---
+++ src/include/storage/index/generic_key.h
@@ -0,0 +1,84 @@
+/**
+ * @file generic_key.h
+ * @author sheep
+ * @brief generic key
+ * @version 0.1
+ * @date 2022-05-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef GENERIC_KEY_H
+#define GENERIC_KEY_H
+
+#include "storage/table/tuple.h"
+#include "type/value.h"
+
+#include <cassert>
+
+namespace TinyDB {
+
+/**
+ * @brief 
+ * brief from bustub:
+ * Generic key is used for indexing with opaque data.
+ * This key type uses an fixed length array to hold data for indexing purposes,
+ * the actual size of which is specified and instantiated with a template argument
+ * @tparam KeySize 
+ */
+template <size_t KeySize>
+class GenericKey {
+public:
+    inline void SetFromKey(const Tuple &tuple) {
+        // initialize to all zero
+        memset(data_, 0, KeySize);
+        assert(tuple.GetLength() < KeySize);
+        memcpy(data_, tuple.GetData(), tuple.GetLength()));
+    }
+
+    inline Value ToValue(Schema *schema, uint32_t column_idx) const {
+        const auto &col = schema->GetColumn(column_idx);
+        if (col.IsInlined()) {
+            return Value::DeserializeFrom(data_ + col.GetOffset());
+        } else {
+            uint32_t offset = *reinterpret_cast<uint32_t *> (data_ + col.GetOffset());
+            return Value::DeserializeFrom(data + offset);
+        }
+    }
+
+    char data_[KeySize];
+};
+
+template<size_t KeySize> 
+class GenericComparator {
+public:
+    explicit GenericComparator(Schema *key_schema) : key_schema_(key_schema) {}
+    GenericComparator(const GenericComparator &other) : key_schema_(other.key_schema_) {}
+
+    inline int operator()(const GenericKey<KeySize> &lhs, const GenericKey<KeySize> &rhs) const {
+        uint32_t column_cnt = key_schema_->GetColumnCount();
+        for (uint32_t i = 0; i < column_cnt; i++) {
+            Value lhs_value = lhs.ToValue(key_schema_, i);
+            Value rhs_value = rhs.ToValue(key_schema_, i);
+
+            if (lhs_value.CompareLessThan(rhs_value) == CmpBool::CmpTrue) {
+                return -1;
+            }
+
+            if (lhs_value.CompareGreaterThan(rhs_value) == CmpBool::CmpTrue) {
+                return 1;
+            }
+
+            // TODO: should we check null?
+            return 0;
+        }
+    }
+
+private:
+    Schema *key_schema_;
+};
+
+}
+
+#endif
--- src/include/storage/index/index.h
+++ src/include/storage/index/index.h
@@ -13,11 +13,18 @@
 #define INDEX_H
 
 #include "catalog/schema.h"
+#include "type/value.h"
+#include "storage/table/tuple.h"
 
 #include <string>
 #include <sstream>
+#include <memory>
+#include <deque>
+#include <unordered_set>
 
 namespace TinyDB {
+
+class Page;
 
 enum IndexType {
     BPlusTree = 0,
@@ -94,6 +101,86 @@ private:
     // this is not necessary since we can generate key_attrs from key_schema and base schema
     const std::vector<uint32_t> key_attrs_;
     IndexType type_;
+};
+
+/**
+ * @brief 
+ * concurrent index execution context.
+ * bustub was using transaction to recording these informations.
+ * but i really want to make these modules isloated, so i decoupled ConcurrentIndexExecutionContext
+ * from transaction
+ */
+struct IndexExecutionContext {
+    // the pages that we latched during index operation
+    std::unique_ptr<std::deque<Page *>> page_set_;
+    // the page IDs that were deleted during index operation
+    std::unique_ptr<std::unordered_set<page_id_t>> deleted_page_set_;
+};
+
+/**
+ * @brief 
+ * base class for different indices. provide the basic interfaces that every index should implement,
+ * which are InsertKey, DeleteKey and ScanKey.
+ */
+class Index {
+public:
+    explicit Index(std::unique_ptr<IndexMetadata> metadata)
+        : metadata_(std::move(metadata)) {}
+    
+    virtual ~Index() {}
+
+    inline IndexMetadata *GetMetadata() const {
+        return metadata_.get();
+    }
+
+    inline int GetIndexColumnCount() const {
+        return metadata_->GetIndexColumnCount();
+    }
+    
+    inline const std::string &GetIndexName() const {
+        return metadata_->GetIndexName();
+    }
+
+    inline Schema *GetKeySchema() const {
+        return metadata_->GetKeySchema();
+    }
+
+    const std::vector<uint32_t> &GetKeyAttrs() const {
+        return metadata_->GetKeyAttrs();
+    }
+
+    std::string ToString() const {
+        return metadata_->ToString();
+    }
+
+    // real operations
+
+    /**
+     * @brief 
+     * insert an entry into index
+     * @param key key to inserted
+     * @param rid value to inserted
+     */
+    virtual void InsertEntry(const Tuple &key, RID rid) = 0;
+
+    /**
+     * @brief 
+     * delete an entry
+     * @param key target key
+     * @param rid target rid, for supporting duplicated keys
+     */
+    virtual void DeleteEntry(const Tuple &key, RID rid) = 0;
+
+    /**
+     * @brief 
+     * get the rid corresponding to given key
+     * @param key target key
+     * @param result result array, for supporting duplicated keys
+     */
+    virtual void ScanKey(const Tuple &key, std::vector<RID> *result) = 0;
+
+private:
+    std::unique_ptr<IndexMetadata> metadata_;
 };
 
 }
