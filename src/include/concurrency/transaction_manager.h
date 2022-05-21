4155a4df886fc45ed34d2bb1eb940dd88a025bde
ysj1173886760 <1173886760@qq.com>
2022-05-21 20:25:06 +0800

    modify api of transaction manager

--- src/include/concurrency/transaction_manager.h
+++ src/include/concurrency/transaction_manager.h
@@ -13,6 +13,10 @@
 #define TRANSACTION_MANAGER_H
 
 #include "concurrency/transaction_context.h"
+#include "storage/table/tuple.h"
+#include "catalog/catalog.h"
+
+#include <atomic>
 
 namespace TinyDB {
 
@@ -38,8 +42,8 @@ public:
      * 
      * @param protocol protocol we want to use
      */
-    TransacionManager(Protocol protocol):
-        protocol_(protocol) {}
+    TransactionManager(Protocol protocol)
+        : protocol_(protocol) {}
     
     // i wonder should we use RID here? since RID represent the disk location and it's bound to 
     // slotted storage format.
@@ -52,9 +56,9 @@ public:
      * @param txn_context 
      * @param[out] tuple tuple that we read
      * @param[in] rid rid of tuple that we want to read
-     * @param[in] table_heap table heap that will perform the actual operation
+     * @param[in] table_info table metadata
      */
-    virtual void Read(TransactionContext *txn_context, Tuple *tuple, RID rid, TableHeap *table_heap) = 0;
+    virtual void Read(TransactionContext *txn_context, Tuple *tuple, RID rid, TableInfo *table_info) = 0;
 
     /**
      * @brief 
@@ -62,18 +66,18 @@ public:
      * @param txn_context 
      * @param tuple tuple that we want to insert
      * @param rid location of new tuple
-     * @param[in] table_heap table heap that will perform the actual operation
+     * @param[in] table_info table metadata
      */
-    virtual void Insert(TransactionContext *txn_context, const Tuple &tuple, RID *rid, TableHeap *table_heap) = 0;
+    virtual void Insert(TransactionContext *txn_context, const Tuple &tuple, RID *rid, TableInfo *table_info) = 0;
 
     /**
      * @brief 
      * Perform Deletion
      * @param txn_context 
      * @param rid rid of the tuple that we want to delete
-     * @param table_heap table heap that will perform the actual operation
+     * @param[in] table_info table metadata
      */
-    virtual void Delete(TransactionContext *txn_context, RID rid, TableHeap *table_heap) = 0;
+    virtual void Delete(TransactionContext *txn_context, RID rid, TableInfo *table_info) = 0;
 
     /**
      * @brief 
@@ -81,15 +85,45 @@ public:
      * @param txn_context 
      * @param tuple new tuple
      * @param rid rid of tuple that we want to update
-     * @param table_heap table heap that will perform the actual operation
+     * @param[in] table_info table metadata
      */
-    virtual void Update(TransactionContext *txn_context, cosnt Tuple &tuple, RID rid, TableHeap *table_heap) = 0;
+    virtual void Update(TransactionContext *txn_context, const Tuple &tuple, RID rid, TableInfo *table_info) = 0;
 
     // sheep: i wonder do we need to provide a scan method?
 
-private:
+    /**
+     * @brief 
+     * Begin a transaction
+     * @param[in] txn_context transaction context to be initialized
+     * @param isolation_level isolation of this transaction
+     */
+    virtual void Begin(TransactionContext *txn_context, IsolationLevel isolation_level = IsolationLevel::READ_COMMITTED) = 0;
+
+    /**
+     * @brief 
+     * Commit a transaction
+     * @param txn_context 
+     */
+    virtual void Commit(TransactionContext *txn_context) = 0;
+
+    /**
+     * @brief 
+     * Abort a transaction
+     * @param txn_context 
+     */
+    virtual void Abort(TransactionContext *txn_context) = 0;
+
+    inline TransactionContext *GetTransaction(txn_id_t txn_id) {
+        return txn_map_[txn_id];
+    }
+
+protected:
     // protocol of this transaction manager
     Protocol protocol_;
+    // transaction id to be assigned
+    std::atomic<txn_id_t> next_txn_id_{0};
+    // txn map
+    std::unordered_map<txn_id_t, TransactionContext *> txn_map_;
 };
 
 }
---
+++ src/include/concurrency/two_phase_locking.h
@@ -0,0 +1,17 @@
+/**
+ * @file two_phase_locking.h
+ * @author sheep
+ * @brief concurrency control -- 2PL
+ * @version 0.1
+ * @date 2022-05-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "concurrency/transaction_context.h"
+#include "concurrency/transaction_manager.h"
+
+namespace TinyDB {
+
+}
