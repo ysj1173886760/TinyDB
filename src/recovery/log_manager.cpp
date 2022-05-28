1f054925ba7af0bb5234bd7956ee800c9e893d53
ysj1173886760 <1173886760@qq.com>
2022-05-28 21:40:35 +0800

    fix log manager

--- src/recovery/log_manager.cpp
+++ src/recovery/log_manager.cpp
@@ -17,7 +17,8 @@ namespace TinyDB {
 lsn_t LogManager::AppendLogRecord(LogRecord *log_record) {
     std::unique_lock<std::mutex> latch(latch_);
     if (log_record->GetSize() + log_size_ > LOG_BUFFER_SIZE) {
-        SwapBuffer();
+        // wait until we have some space to insert the log
+        operation_cv_.wait(latch, [&]() { return log_record->GetSize() + log_size_ <= LOG_BUFFER_SIZE; });
     }
 }
 
@@ -25,23 +26,25 @@ void LogManager::FlushThread() {
     while (enable_flushing_.load()) {
         std::unique_lock<std::mutex> latch(latch_);
         // wait until log timeout or buffer is full or forcing log is needed
-        if (!flush_cv_.wait_for(latch, log_timeout, [&] { return need_flush_ == true; })) {
-            // if need flush is false, it means that we are triggered by time out.
-            // thus we need to perform swapping buffer manually
-            SwapBuffer();
-        }
+        flush_cv_.wait_for(latch, log_timeout, [&] { return need_flush_ == true; });
+        // TODO: wait until previous log has been flushed
+        // swap buffer
+        SwapBuffer();
         // remember the biggest lsn in buffer
         lsn_t lsn = next_lsn_ - 1;
         // save the buffer pointer and buffer size
         uint32_t flush_size = flush_size_;
         char *flush_buffer = flush_buffer_;
-        // unlock the latch since buffer has been swapped
+        // unlock the latch since we don't want to flush the disk while holding the lock
         latch.unlock();
+        // resume the append log record operation 
+        operation_cv_.notify_one();
         // start flushing log
         disk_manager_->WriteLog(flush_buffer, flush_size);
-
         // store persistent_lsn
         persistent_lsn_.store(lsn);
+        // TODO: set need flush to false then wakeup the Flush call
+        // i think we should use lsn to determine it
     }
 }
 
